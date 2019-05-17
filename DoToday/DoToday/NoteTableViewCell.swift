56edaccadacce6ed43783b9605e5b679f68d2745
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-05-16 21:55:33 -0400

    update storyboard and create custom table view cell class and set the cell to it

---
+++ DoToday/DoToday/NoteTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  NoteTableViewCell.swift
+//  DoToday
+//
+//  Created by Julian A. Fordyce on 5/16/19.
+//  Copyright © 2019 Glas Labs. All rights reserved.
+//
+
+import UIKit
+
+class NoteTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
