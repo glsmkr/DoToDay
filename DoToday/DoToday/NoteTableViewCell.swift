0e2e5941a5a20d9d5f5b0bec2b437c5ebb15163c
Julian A. Fordyce <julian.a.fordyce@gmail.com>
2019-05-25 20:23:09 -0400

    created an outlet from label to the note table view cell file

--- DoToday/DoToday/NoteTableViewCell.swift
+++ DoToday/DoToday/NoteTableViewCell.swift
@@ -10,15 +10,10 @@ import UIKit
 
 class NoteTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
-    }
-
+    
+    // MARK: - Properties
+    @IBOutlet weak var notesLabel: UILabel!
+    
 }
